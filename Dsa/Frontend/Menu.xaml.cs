99a6dec61713534646aa2bdedf6573ef008b73b9
Yannick Hassler <yhassler@gmx.ch>
2019-11-06 10:18:20 +0100

    Added Frontend Project

---
+++ Dsa/Frontend/Menu.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Frontend
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Menu : Window
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+    }
+}
