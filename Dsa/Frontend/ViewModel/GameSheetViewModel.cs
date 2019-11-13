dd6e9aaa662e19c58ec02c47234b053c7303f375
Yannick Hassler <yhassler@gmx.ch>
2019-11-13 10:24:20 +0100

    Added all UI Elements

---
+++ Dsa/Frontend/ViewModel/GameSheetViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+
+namespace Frontend.ViewModel
+{
+    class GameSheetViewModel
+    {
+        private int _ones;
+        public int Ones
+        {
+            get { return _ones; }
+//          set { SettingsProperty(ref _ones, value); }
+        }
+    }
+}
