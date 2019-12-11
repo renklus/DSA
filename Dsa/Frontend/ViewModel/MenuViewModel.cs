dd96ca5e99878a1ed9a815d5b1f836763fabf10b
Yannick Hassler <yhassler@gmx.ch>
2019-12-11 08:59:52 +0100

    Fixed Game joining

---
+++ Dsa/Frontend/ViewModel/MenuViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Frontend.Commands;
+
+namespace Frontend.ViewModel
+{
+    class MenuViewModel : BindableBase
+    {
+        public RelayCommand GoToCommand { get; set; }
+
+        public MenuViewModel()
+        {
+            GoToCommand = new RelayCommand(GoTo);
+        }
+
+        public void GoTo()
+        {
+            
+        }
+    }
+}
